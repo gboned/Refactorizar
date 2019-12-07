8f8edd084177798addb0393ab19f360f13b821ec
gboned <gboned@cifpfbmoll.eu>
2019-12-07 03:28:24 +0100

    refactor: Refactorizando one pair & test
    Refactorizando one pair, cambiándole el nombre y cambiando el caso test
    correspondiente

--- test_yatzy.py
+++ test_yatzy.py
@@ -55,9 +55,9 @@ def test_sixes_test():
 
 
 def test_one_pair():
-    assert 6 == Yatzy.score_pair(3, 4, 3, 5, 6)
-    assert 10 == Yatzy.score_pair(5, 3, 3, 3, 5)
-    assert 12 == Yatzy.score_pair(5, 3, 6, 6, 5)
+    assert 6 == Yatzy.one_pair(3, 4, 3, 5, 6)
+    assert 10 == Yatzy.one_pair(5, 3, 3, 3, 5)
+    assert 12 == Yatzy.one_pair(5, 3, 6, 6, 5)
 
 
 def test_two_Pair():
