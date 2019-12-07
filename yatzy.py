0827f28b1e3d992be47d5d070706a5640b01135a
gboned <gboned@cifpfbmoll.eu>
2019-12-07 20:22:51 +0100

    refactor: Refactorizando three of a kind
    Refactorizando three of a kind, creando una lista que añada los números
    que se repiten más de 3 veces, y que devuelva la suma de los 3 números

--- yatzy.py
+++ yatzy.py
@@ -90,17 +90,12 @@ class Yatzy:
             return 0
 
     @staticmethod
-    def three_of_a_kind(d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
+    def three_of_a_kind(*dice):
+        num = []
+        for d in dice:
+            if dice.count(d) >= 3:
+                num.append(d)
+            return sum(set(num)) * 3
 
     @staticmethod
     def four_of_a_kind(_1,  _2,  d3,  d4,  d5):
