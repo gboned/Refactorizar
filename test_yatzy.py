63b4929de3df3379b599458efb7c42e2ff2d77e3
gboned <gboned@cifpfbmoll.eu>
2019-12-11 01:05:09 +0100

    refactor: Refactorizado test four_of_a_kind
    Refactorizando test four_of_a_kind reordenando los asserts

--- test_yatzy.py
+++ test_yatzy.py
@@ -68,10 +68,10 @@ def test_three_of_a_kind():
 
 
 def test_four_of_a_kind():
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 5)
-    assert 20 == Yatzy.four_of_a_kind(5, 5, 5, 4, 5)
-    assert 12 == Yatzy.four_of_a_kind(3, 3, 3, 3, 3)
-    assert 0 == Yatzy.four_of_a_kind(3, 3, 3, 2, 1)
+    assert Yatzy.four_of_a_kind(3, 3, 3, 3, 5) == 12
+    assert Yatzy.four_of_a_kind(5, 5, 5, 4, 5) == 20
+    assert Yatzy.four_of_a_kind(3, 3, 3, 3, 3) == 12
+    assert Yatzy.four_of_a_kind(3, 3, 3, 2, 1) == 0
 
 
 def test_smallStraight():
